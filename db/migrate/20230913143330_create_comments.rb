f59dce3528d596f9423abf34ac348caa85da5713
kessie <kayp3477@gmail.com>
2023-09-13 16:49:08 +0000

    Generate migration for Comment model database table

---
+++ db/migrate/20230913143330_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :text
+      t.integer :user_id
+      t.integer :post_id
+
+      t.timestamps
+    end
+    add_foreign_key :comments, :users
+    add_foreign_key :comments, :posts
+  end
+end
